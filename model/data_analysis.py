95514627679e3db4ac7bd4d6e2bfe0eb2664230e
madhuradhawangale00-sys <madhuradhawangale00@gmail.com>
2026-08-19 23:28:03 +0530

    analysed dataset

---
+++ model/data_analysis.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+# Load dataset
+df = pd.read_csv("dataset\data.csv")
+
+print("\n========== FIRST 5 ROWS ==========")
+print(df.head())
+
+print("\n========== DATASET SHAPE ==========")
+print(df.shape)
+
+print("\n========== COLUMN NAMES ==========")
+print(df.columns.tolist())
+
+print("\n========== DATA TYPES ==========")
+print(df.dtypes)
+
+print("\n========== MISSING VALUES ==========")
+print(df.isnull().sum())
+
+print("\n========== DUPLICATE ROWS ==========")
+print(df.duplicated().sum())
+
+print("\n========== BASIC STATISTICS ==========")
+print(df.describe())
